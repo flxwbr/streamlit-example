7472e8c13d545e6926dd4200e24da4029c1f7d2f
Weber <weber@win.iwm.rwth-aachen.de>
2024-01-05 13:32:18 +0100

    upload image

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import cv2
 
 """
 # Welcome to Streamlit!
@@ -13,6 +14,11 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+uploaded_file = st.file_uploader("Choose af file")
+
+if uploaded_file is not None:
+    original_image = cv2.imread(uploaded_file)
+    st.image(uploaded_file, caption='Image description')
 
 
 # num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
